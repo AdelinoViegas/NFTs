14d34eec4bf7cddc0795f683d7f74f6aa14f66b8
AdelinoViegas <buchoteclas@gmail.com>
2025-02-11 22:20:16 +0100

    actualizando os componentes

--- src/components/section2.js
+++ src/components/section2.js
@@ -1,47 +1,47 @@
 import Banner from "../img/banner.png";
 
-export default function section2(){
- return(
-   <section className="section2">
-    <div className="number">
-		<div>
-			<span>10K+</span>
-			<p>Artes</p>
-		</div>
-		<div>
-			<span>200+</span>
-			<p>Vendas</p>
-		</div>
-		<div>
-			<span>20</span>
-			<p>Artistas</p>
-		</div>
-	</div>
-	<div className="info-item">
-		<div className="phrase">
-			<div>
-				<p>Compre e utilize os nossos óculos com uma qualidade 
-          rica e confortável que permitirá ver com uma eficiência
-          tem uma <span>resistência</span> boa adapta-se a varios ambientes seja quente ou frio adquira e desfrute desse produto. 
-				</p>
-				<p>
-					Óculos confortáveis.
-				</p>
+export default function section2() {
+	return (
+		<section className="section2">
+			<div className="number">
+				<div>
+					<span>10K+</span>
+					<p>Artes</p>
+				</div>
+				<div>
+					<span>200+</span>
+					<p>Vendas</p>
+				</div>
+				<div>
+					<span>20</span>
+					<p>Artistas</p>
+				</div>
 			</div>
-			<div>
-				<p>Compre e utilize os nossos óculos com uma qualidade 
-          rica e confortável que permitirá ver com uma eficiência
-          tem uma <span>resistência</span> boa adapta-se a varios ambientes seja quente ou frio adquira e desfrute desse produto. 
-				</p>
-				<p>
-					Óculos confortáveis.
-				</p>
+			<div className="info-item">
+				<div className="phrase">
+					<div>
+						<p>Compre e utilize os nossos óculos com uma qualidade
+							rica e confortável que permitirá ver com uma eficiência
+							tem uma <span>resistência</span> boa adapta-se a varios ambientes seja quente ou frio adquira e desfrute desse produto.
+						</p>
+						<p>
+							Óculos confortáveis.
+						</p>
+					</div>
+					<div>
+						<p>Compre e utilize os nossos óculos com uma qualidade
+							rica e confortável que permitirá ver com uma eficiência
+							tem uma <span>resistência</span> boa adapta-se a varios ambientes seja quente ou frio adquira e desfrute desse produto.
+						</p>
+						<p>
+							Óculos confortáveis.
+						</p>
+					</div>
+				</div>
+				<div className="banner">
+					<img src={Banner} alt="banner" />
+				</div>
 			</div>
-		</div>
-		<div className="banner">
-			<img src={Banner} alt="banner"/>
-		</div>
-	</div>
-   </section>
- );
+		</section>
+	);
 }
